68531b4bb30e25cea3181a257803e683f2062f91
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-11-22 20:49:58 -0500

    Add Lesson_1_Representation_Of_Memory_In_C++/part_1_memory_address_demo.cpp

---
+++ Lesson_1_Representation_Of_Memory_In_C++/part_1_memory_address_demo.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 10; // Allocates memory for an integer
+    cout << "Address of x: " << &x << endl;
+    cout << "Value of x: " << x << endl;
+
+    return 0;
+}
