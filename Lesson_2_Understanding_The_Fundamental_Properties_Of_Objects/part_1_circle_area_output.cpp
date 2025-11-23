074a2e7716d0057954543e056279e8b926bfb7ac
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-11-22 20:51:09 -0500

    Add Lesson_2_Understanding_The_Fundamental_Properties_Of_Objects/part_1_circle_area_output.cpp

---
+++ Lesson_2_Understanding_The_Fundamental_Properties_Of_Objects/part_1_circle_area_output.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+class Circle {
+public:
+    double radius;
+
+    Circle(double r) : radius(r) {}
+
+    double getArea() {
+        return 3.14159 * radius * radius;
+    }
+};
+
+int main() {
+    Circle myCircle(5.0);
+    cout << "Area of Circle: " << myCircle.getArea() << endl;
+    
+    // Retrieving the address of myCircle
+    cout << "Address of myCircle: " << &myCircle << endl;
+    return 0;
+}
